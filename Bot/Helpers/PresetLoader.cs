6618c10e9a633486288180e4a345fa56a9033779
berichan <66521620+berichan@users.noreply.github.com>
2021-03-09 02:41:12 +0000

    Presetting husk

---
+++ Bot/Helpers/PresetLoader.cs
@@ -0,0 +1,32 @@
+﻿using NHSE.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SysBot.Base;
+
+namespace SysBot.ACNHOrders
+{
+    public static class PresetLoader
+    {
+        public static Item[]? GetPreset(string nhiPath)
+        {
+            if (!File.Exists(nhiPath))
+            {
+                LogUtil.LogInfo($"{nhiPath} does not exist.", nameof(PresetLoader));
+                return null;
+            }
+
+            var fileBytes = File.ReadAllBytes(nhiPath);
+            if (fileBytes.Length > (Item.SIZE * 40) || fileBytes.Length == 0 || fileBytes.Length % 8 != 0)
+            {
+                LogUtil.LogInfo($"{nhiPath} is an invalid size for an NHI file.", nameof(PresetLoader));
+                return null;
+            }
+
+            return Item.GetArray(fileBytes);
+        }
+    }
+}
