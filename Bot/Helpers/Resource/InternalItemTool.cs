f53776e4bd28b20c14bf0a92633062a19b7335f1
berichan <66521620+berichan@users.noreply.github.com>
2021-03-15 21:15:30 +0000

    Use updated internal item list instead of hardcoded values.

---
+++ Bot/Helpers/Resource/InternalItemTool.cs
@@ -0,0 +1,33 @@
+﻿using NHSE.Core;
+using System;
+using System.Collections.Generic;
+
+namespace SysBot.ACNHOrders
+{
+    public class InternalItemTool
+    {
+        private readonly List<ushort> InternalItems;
+
+        public static InternalItemTool CurrentInstance = new();
+
+        public InternalItemTool()
+        {
+            var items = FileUtil.GetEmbeddedResource("SysBot.ACNHOrders.Resources", "InternalHexList.txt");
+            var spl = items.Split(new string[3] { "\r\n", "\r", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            InternalItems = new List<ushort>();
+            foreach (var it in spl)
+                InternalItems.Add(ushort.Parse(it, System.Globalization.NumberStyles.HexNumber));
+        }
+
+        public bool IsInternalItem(ushort val) => InternalItems.Contains(val);
+
+        public bool IsSane(IReadOnlyCollection<Item> items)
+        {
+            foreach (var it in items)
+                if (IsInternalItem(it.ItemId))
+                    return false;
+            return true;
+        }
+    }
+}
--- Bot/Helpers/Static/FileUtil.cs
+++ Bot/Helpers/Static/FileUtil.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,6 +15,22 @@ namespace SysBot.ACNHOrders
             {
                 await sourceStream.WriteAsync(bytes, 0, bytes.Length, token).ConfigureAwait(false);
             };
+        }
+
+        public static string GetEmbeddedResource(string namespacename, string filename)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            if (assembly == null)
+                return string.Empty;
+            var resourceName = namespacename + "." + filename;
+#pragma warning disable CS8600, CS8604
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
+                return result;
+            }
+#pragma warning restore CS8600, CS8604
         }
     }
 }
